f6d028cf49f75d4f0e136f11e63b275c63ab0158
prasthi <33329490+prasthi@users.noreply.github.com>
2017-11-03 15:06:05 -0400

    Update deploy_dockerhub.sh

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -1,1 +1,9 @@
-#!/bin/shdocker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSif [ "$TRAVIS_BRANCH" = "master" ]; then    TAG="latest"else    TAG="$TRAVIS_BRANCH"fidocker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .docker push $TRAVIS_REPO_SLUG
+#!/bin/sh
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker push $TRAVIS_REPO_SLUG
