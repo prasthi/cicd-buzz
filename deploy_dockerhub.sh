05deddf719fa5bf9cd67768f1e878da9bea0cbe0
prasthi <33329490+prasthi@users.noreply.github.com>
2017-11-03 15:04:54 -0400

    Create deploy_dockerhub.sh

---
+++ deploy_dockerhub.sh
@@ -0,0 +1,1 @@
+#!/bin/shdocker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSif [ "$TRAVIS_BRANCH" = "master" ]; then    TAG="latest"else    TAG="$TRAVIS_BRANCH"fidocker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .docker push $TRAVIS_REPO_SLUG
